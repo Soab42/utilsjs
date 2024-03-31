e8654fdf78eae80fe0fdf289ca7147c2e0e15f1f
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-03-31 10:46:52 +0600

    fix blog card

--- src/components/components/blog/MainCard.jsx
+++ src/components/components/blog/MainCard.jsx
@@ -5,46 +5,47 @@ import { Link } from "react-router-dom";
 // import ActionDot from "../common/ActionDot";
 import CardAuthor from "./CardAuthor";
 export default function MainCard({ data = {}, postId }) {
-    const ref = useRef(null);
-    const isInView = useInView(ref, {
-        once: true,
-        margin: "0px 100px -50px 0px",
-    });
-    return (
-        <div className="relative w-[calc(33.33%-8px)]">
-            <Link to={`${data.name}/${postId}`}>
-                <div
-                    className="blog-card"
-                    ref={ref}
-                    key={data.id}
-                    style={{
-                        background: isInView ? "" : "white",
-                        transform: !isInView
-                            ? "translateY(30px)"
-                            : "translateX(0px)",
-                        opacity: isInView ? 1 : 0,
-                        transition:
-                            "all 1s cubic-bezier(0.25, 0.1, 0.25, 1) .1s",
-                    }}>
-                    <div className="relative">
-                        <h3 className="dark:text-slate-700 text-xl lg:text-2xl">
-                            {data.name}
-                        </h3>
-                        <p
-                            className="mb-6 text-base text-slate-500 mt-1 line-clamp-5"
-                            dangerouslySetInnerHTML={{ __html: data.content }}>
-                            {/* {data.content} */}
-                        </p>
+  const ref = useRef(null);
+  const isInView = useInView(ref, {
+    once: true,
+    margin: "0px 100px -50px 0px",
+  });
+  return (
+    <div className="relative w-[calc(33.33%-8px)]">
+      <Link to={`${data.name}/${postId}`}>
+        <div
+          className="blog-card h-full "
+          ref={ref}
+          key={data.id}
+          style={{
+            background: isInView ? "" : "white",
+            transform: !isInView ? "translateY(30px)" : "translateX(0px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 1s cubic-bezier(0.25, 0.1, 0.25, 1) .1s",
+          }}
+        >
+          <div className="relative flex flex-col justify-between">
+            <div>
+              <h3 className="dark:text-slate-700 text-xl lg:text-2xl">
+                {data.name}
+              </h3>
+              <p
+                className="mb-6 text-base text-slate-500 mt-1 line-clamp-5"
+                dangerouslySetInnerHTML={{ __html: data.content }}
+              >
+                {/* {data.content} */}
+              </p>
+            </div>
 
-                        <CardAuthor
-                            author={data.author}
-                            likes={data.likes}
-                            createdAt={data.createdAt}
-                        />
-                    </div>
-                </div>
-            </Link>
-            {/* {isMe && <ActionDot post={data} />} */}
+            <CardAuthor
+              author={data.author}
+              likes={data.likes}
+              createdAt={data.createdAt}
+            />
+          </div>
         </div>
-    );
+      </Link>
+      {/* {isMe && <ActionDot post={data} />} */}
+    </div>
+  );
 }
